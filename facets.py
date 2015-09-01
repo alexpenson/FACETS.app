b96b3b0492f0f8ee8b9f66e5b23f276cdb799f5f
Alex Penson <avp2106@columbia.edu>
2015-09-01 17:12:49 -0400

    Update facets.py

--- facets.py
+++ facets.py
@@ -6,16 +6,6 @@ import argparse, os, sys, re, subprocess, itertools, errno, csv, gzip
 SDIR = os.path.dirname(os.path.realpath(__file__))
 
 
-# def make_sure_path_exists(path):
-#     """
-#     http://stackoverflow.com/questions/273192/in-python-check-if-a-directory-exists-and-create-it-if-necessary
-#     """
-#     try:
-#         os.makedirs(path)
-#     except OSError as exception:
-#         if exception.errno != errno.EEXIST:
-#             raise
-
 def make_sure_path_exists(directory):
     if not os.path.exists(directory):
         os.makedirs(directory) 
