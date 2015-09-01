3e10a350b7b77373606ee0c71b26e3cf40dc9972
Alexander Penson <pensona@cbio.mskcc.org>
2015-09-01 16:31:07 -0400

    check for existance of counts files, added fromcounts subcommand

--- facets.py
+++ facets.py
@@ -20,6 +20,14 @@ def make_sure_path_exists(directory):
     if not os.path.exists(directory):
         os.makedirs(directory) 
 
+def gzip_file_with_size(file_path):
+    if not os.path.exists(file_path): return(False)
+    with gzip.open(file_path, 'rb') as f:
+        for i, l in enumerate(f):
+            i = i+1
+            if i > 10: return(True)
+        return(False)
+
         
 def slugify(value):
     """
@@ -63,32 +71,39 @@ def runlsf(args):
 
 
         ### CHECK FOR EXISTENCE OF COUNTS FILES
-        # with gzip.open('file.txt.gz', 'rb') as f:
-        #     file_content = f.read()
+        n_countsfile_exists = gzip_file_with_size(n_countsfile)
+        t_countsfile_exists = gzip_file_with_size(t_countsfile)
+        countsMerged_file_exists = gzip_file_with_size(countsMerged_file)
 
 
         ### BUILD LIST OF COMMANDS TO BE EXECUTED (WHILE WAITING FOR ERRORS)
                 
         ### GET BASE COUNTS
-        
+
+        wait_string = '' ### string to add to later bsub commands
+
         counts_cmd = ('bsub -We 59 -o Log/ -e Err/ -J %s_count_%s -R "rusage[mem=40]" ' 
                       '%s/bin/getBaseCountsZZAutoWithName.sh %s %s')
         
-        n_counts_cmd = counts_cmd % ("n", Tumor_Sample_Barcode, SDIR, n_countsfile, n_bamfile)
-        cmd_list.append(n_counts_cmd)
-        t_counts_cmd = counts_cmd % ("t", Tumor_Sample_Barcode, SDIR, t_countsfile, t_bamfile)        
-        cmd_list.append(t_counts_cmd)
-        
+        if not n_countsfile_exists and not countsMerged_file_exists:
+            n_counts_cmd = counts_cmd % ("n", Tumor_Sample_Barcode, SDIR, n_countsfile, n_bamfile)
+            cmd_list.append(n_counts_cmd)
+            wait_string = '-w "post_done(n_count_%s) && post_done(t_count_%s)"' % (Tumor_Sample_Barcode, Tumor_Sample_Barcode)
+            
+        if not t_countsfile_exists and not countsMerged_file_exists:
+            t_counts_cmd = counts_cmd % ("t", Tumor_Sample_Barcode, SDIR, t_countsfile, t_bamfile)        
+            cmd_list.append(t_counts_cmd)
+            wait_string = '-w "post_done(n_count_%s) && post_done(t_count_%s)"' % (Tumor_Sample_Barcode, Tumor_Sample_Barcode)
+                    
         
         ### MERGE COUNTS
-        ## wait_string = ''
-        wait_string = '-w "post_done(*_count_%s)"' % (Tumor_Sample_Barcode)
+        if not countsMerged_file_exists:
+            merge_cmd = ('bsub -We 59 -o Log/ -e Err/ -n 2 -R "rusage[mem=60]" -J merge_%s %s '
+                         '"%s/bin/mergeTN.R %s %s | gzip -9 -c > %s"')
+            merge_cmd = merge_cmd % (Tumor_Sample_Barcode, wait_string, SDIR, t_countsfile, n_countsfile, countsMerged_file)
+            cmd_list.append(merge_cmd)
+            wait_string = '-w "post_done(merge_%s)"' % (Tumor_Sample_Barcode)
         
-        merge_cmd = ('bsub -We 59 -o Log/ -e Err/ -n 2 -R "rusage[mem=60]" -J merge_%s %s '
-                     '"%s/bin/mergeTN.R %s %s | gzip -9 -c > %s"')
-        merge_cmd = merge_cmd % (Tumor_Sample_Barcode, wait_string, SDIR, t_countsfile, n_countsfile, countsMerged_file)
-        cmd_list.append(merge_cmd)
-
         # ### NORM COUNTS
         # norm_cmd = ('bsub -We 59 -o Log/ -e Err/ -n 2 -R "rusage[mem=60]" -J norm_%s %s '
         #              '"%s/bin/norm_normal_depth.R %s | gzip -9 -c > %s"')
@@ -97,10 +112,8 @@ def runlsf(args):
 
 
         ### DO FACETS
-        wait_string = '-w "post_done(merge_%s)"' % (Tumor_Sample_Barcode)
         doFacets_cmd = ('bsub -We 59 -o LSF/ -e Err/ -J facets_%s %s '
                         '%s/bin/doFacets.R %s %s')
-        
         doFacets_cmd = doFacets_cmd % (Tumor_Sample_Barcode, wait_string, SDIR, " ".join(args.facets_args), countsMerged_file)
         cmd_list.append(doFacets_cmd)
         
@@ -115,13 +128,23 @@ def runlsf(args):
             make_sure_path_exists("cval_50/" + Tumor_Sample_Barcode)
             
         for cmd in cmd_list:
-            # ###            subprocess.call(cmd) ### why doesn't this work?
-            subprocess.call(cmd.split(" "))
+            subprocess.call(["echo", cmd])
+            subprocess.call(cmd, shell=True)
             #os.system(cmd)
 
 def fromcounts(args):
-    print "run " + SDIR + "/bin/doFacets.R"
-    
+    ### BUILD COMMAND LIST
+    for countsMerged_file in args.counts_files:
+        doFacets_cmd = ('bsub -We 59 -o LSF/ -e Err/ -J facets_%s %s '
+                        '%s/bin/doFacets.R %s %s')
+        doFacets_cmd = doFacets_cmd % (Tumor_Sample_Barcode, wait_string, SDIR, " ".join(args.facets_args), countsMerged_file)
+        cmd_list.append(doFacets_cmd)
+
+    ### EXECUTE COMMAND LIST
+    for cmd in cmd_list:
+        subprocess.call(["echo", cmd])
+        subprocess.call(cmd, shell=True)
+
 # def norm(args):
 #     print "run " + SDIR + "/bin/norm_normal_depth.R"
             
